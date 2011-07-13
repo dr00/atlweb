134d0c1db002609609bdb45996bb82010d43c624
dom <dom@domxps.(none)>
2011-07-13 15:11:07 -0700

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlweb::Application.config.secret_token = 'b16b9d25c017c79e95c3be9b4172c6222ac5a5b1cb5238fcab8d586888ab16190d92c331591ce0df3d96d34a2a905dc372949e233895ef8b724614c63eda00ea'
