0a8408accb02d8e0d66190adab470dabb0cd92e0
= <=>
2011-07-26 22:59:29 -0700

    clearing out old, deleted files

--- app/models/job_nodes.rb
+++
@@ -1,2 +0,0 @@
-class JobNodes < ActiveRecord::Base
-end
--- app/models/node.rb
+++
@@ -1,4 +0,0 @@
-class Node < ActiveRecord::Base
-  validates :ipaddress, :presence => true
-  validates :name, :presence => true, :uniqueness => true
-end
